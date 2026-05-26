516d9867a38d376a0eec380266cf7d3ee356265b
arun097th-collab <arun097th@gmail.com>
2026-05-26 23:29:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,79 +1,10 @@
 from flask import Flask
-from pyrogram import Client, filters
-import asyncio
-
-# ======================
-# FLASK
-# ======================
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Bot Running"
-
-# ======================
-# TELEGRAM API
-# ======================
-
-API_ID = 21295053
-API_HASH = "297598578931dcc642c2519414079f8e"
-BOT_TOKEN = "8852863411:AAHJeN2b7oHdWedNjG1wTb0uNYSSgs3JK4A"
-
-# ======================
-# BOT
-# ======================
-
-bot = Client(
-    "mybot",
-    api_id=API_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN
-)
-
-# ======================
-# START COMMAND
-# ======================
-
-@bot.on_message(filters.command("start"))
-async def start(client, message):
-
-    await message.reply_text(
-        "🔥 Bot Working Successfully"
-    )
-
-# ======================
-# VIDEO
-# ======================
-
-@bot.on_message(filters.video | filters.document)
-async def video(client, message):
-
-    await message.reply_text(
-        "✅ Video Received"
-    )
-
-# ======================
-# MAIN
-# ======================
-
-async def main():
-
-    await bot.start()
-
-    print("Bot Started")
-
-    while True:
-        await asyncio.sleep(1)
+    return "Working"
 
 if __name__ == "__main__":
-
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-
-    loop.create_task(main())
-
-    app.run(
-        host="0.0.0.0",
-        port=10000
-    )
+    app.run(host="0.0.0.0", port=10000)

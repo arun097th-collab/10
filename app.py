3cf180806fe87d855c01be885011c1dcfc7c6073
arun097th-collab <arun097th@gmail.com>
2026-05-26 23:21:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 from flask import Flask
 from pyrogram import Client, filters
-import threading
 import asyncio
+
+# ======================
+# FLASK
+# ======================
 
 app = Flask(__name__)
 
-# =========================
+@app.route("/")
+def home():
+    return "Bot Running"
+
+# ======================
 # TELEGRAM API
-# =========================
+# ======================
 
 API_ID = 21295053
 API_HASH = "297598578931dcc642c2519414079f8e"
 BOT_TOKEN = "8852863411:AAHJeN2b7oHdWedNjG1wTb0uNYSSgs3JK4A"
 
-# =========================
+# ======================
 # BOT
-# =========================
+# ======================
 
 bot = Client(
     "mybot",
@@ -24,28 +31,20 @@ bot = Client(
     bot_token=BOT_TOKEN
 )
 
-# =========================
-# HOME
-# =========================
-
-@app.route("/")
-def home():
-    return "Bot Running Successfully"
-
-# =========================
+# ======================
 # START COMMAND
-# =========================
+# ======================
 
 @bot.on_message(filters.command("start"))
 async def start(client, message):
 
     await message.reply_text(
-        "🔥 Send Video Up To 2GB"
+        "🔥 Bot Working Successfully"
     )
 
-# =========================
-# VIDEO MESSAGE
-# =========================
+# ======================
+# VIDEO
+# ======================
 
 @bot.on_message(filters.video | filters.document)
 async def video(client, message):
@@ -54,28 +53,25 @@ async def video(client, message):
         "✅ Video Received"
     )
 
-# =========================
-# BOT THREAD FIX
-# =========================
+# ======================
+# MAIN
+# ======================
 
-def run_bot():
+async def main():
+
+    await bot.start()
+
+    print("Bot Started")
+
+    while True:
+        await asyncio.sleep(1)
+
+if __name__ == "__main__":
 
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
 
-    bot.run()
-
-# =========================
-# START BOT THREAD
-# =========================
-
-threading.Thread(target=run_bot).start()
-
-# =========================
-# RUN FLASK
-# =========================
-
-if __name__ == "__main__":
+    loop.create_task(main())
 
     app.run(
         host="0.0.0.0",
